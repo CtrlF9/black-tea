8f00e0a7ce058271c0f0ff971bded5e115f55a24
CtrlF9 <spsureshkumar8586@gmail.com>
2019-03-24 17:14:11 +0530

    login page basic design done without any actions

--- app/src/index.js
+++ app/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Login from "./login"
 
-ReactDOM.render(<Login/>,document.body);
+ReactDOM.render(<Login/>,document.getElementById("root"));
--- app/src/login.js
+++ app/src/login.js
@@ -1,13 +1,16 @@
 import React,{ Component } from "react";
+import "./css/common.css";
+import "./css/login.css";
+
 class Login extends Component{
     render(){
         return(
-        <section class="loginForm">
+        <section className="loginForm">
             <input type="text" placeholder="user name or e-mail" />
             <br/>
             <input type="password" placeholder="password" />
             <br/>
-            <input type="button" class="btn-login" value="login" />
+            <input type="button" className="btn-login" value="login" />
         </section>
         );
     }
